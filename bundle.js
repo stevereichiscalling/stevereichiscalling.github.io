269d3097743536c92d3eeedadd27c07131a36896
Seth Kranzler <sethkranzler@gmail.com>
2017-02-08 18:35:34 -0500

    adds button and responsive stuff

--- bundle.js
+++ bundle.js
@@ -46,24 +46,24 @@
 
 	var Tone = __webpack_require__(1);
 
-	function onEnded(that){
-	  that.play();
-	  that.className = "shake shake-constant";
-	  setTimeout(function(){
-	    that.className = "";
-	  }.bind(that), 300);
-	}
+	// function onEnded(that){
+	//   that.play();
+	//   that.className = "shake shake-constant";
+	//   setTimeout(function(){
+	//     that.className = "";
+	//   }.bind(that), 300);
+	// }
 
 	var rightVid = document.getElementById("video-right");
 	rightVid.playbackRate = 0.99;
-	rightVid.addEventListener('ended', function(){
-	  onEnded(this);
-	});
+	// rightVid.addEventListener('ended', function(){
+	//   onEnded(this);
+	// });
 
 	var leftVid = document.getElementById("video-left");
-	leftVid.addEventListener('ended', function(){
-	  onEnded(this);
-	});
+	// leftVid.addEventListener('ended', function(){
+	//   onEnded(this);
+	// });
 
 	var left = new Tone.Panner(-1).toMaster();
 	var right = new Tone.Panner(1).toMaster();
@@ -77,9 +77,24 @@
 
 	Tone.Buffer.on("load", function(){
 	  rightPlayer.buffer = leftPlayer.buffer;
+	  if(window.innerWidth > 769){
+	    leftPlayer.start();
+	    rightPlayer.start();
+	    leftVid.play();
+	    rightVid.play();
+	  }
+	});
+
+	var button = document.getElementById("start-button");
+
+	button.addEventListener("click", function(){
 	  leftPlayer.start();
 	  rightPlayer.start();
-	});
+	  rightVid.play();
+	  leftVid.play();
+	  leftVid.className = "";
+	  button.style.display = "none";
+	})
 
 /***/ },
 /* 1 */
--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 var Tone = require("tone");
 
-function onEnded(that){
-  that.play();
-  that.className = "shake shake-constant";
-  setTimeout(function(){
-    that.className = "";
-  }.bind(that), 300);
-}
+// function onEnded(that){
+//   that.play();
+//   that.className = "shake shake-constant";
+//   setTimeout(function(){
+//     that.className = "";
+//   }.bind(that), 300);
+// }
 
 var rightVid = document.getElementById("video-right");
 rightVid.playbackRate = 0.99;
-rightVid.addEventListener('ended', function(){
-  onEnded(this);
-});
+// rightVid.addEventListener('ended', function(){
+//   onEnded(this);
+// });
 
 var leftVid = document.getElementById("video-left");
-leftVid.addEventListener('ended', function(){
-  onEnded(this);
-});
+// leftVid.addEventListener('ended', function(){
+//   onEnded(this);
+// });
 
 var left = new Tone.Panner(-1).toMaster();
 var right = new Tone.Panner(1).toMaster();
@@ -31,6 +31,21 @@ rightPlayer.playbackRate = 0.99;
 
 Tone.Buffer.on("load", function(){
   rightPlayer.buffer = leftPlayer.buffer;
+  if(window.innerWidth > 769){
+    leftPlayer.start();
+    rightPlayer.start();
+    leftVid.play();
+    rightVid.play();
+  }
+});
+
+var button = document.getElementById("start-button");
+
+button.addEventListener("click", function(){
   leftPlayer.start();
   rightPlayer.start();
-});
+  rightVid.play();
+  leftVid.play();
+  leftVid.className = "";
+  button.style.display = "none";
+})
