368e64ee2736cceeaf534ddc5b2e3a552348a95a
Seth Kranzler <sethkranzler@gmail.com>
2017-02-25 19:04:26 -0500

    adds message thing

--- js/bundle.js
+++ js/bundle.js
@@ -46,29 +46,15 @@
 
 	var Tone = __webpack_require__(1);
 
-	// function onEnded(that){
-	//   that.play();
-	//   that.className = "shake shake-constant";
-	//   setTimeout(function(){
-	//     that.className = "";
-	//   }.bind(that), 300);
-	// }
-
 	var rightVid = document.getElementById("video-right");
 	rightVid.playbackRate = 0.99;
-	// rightVid.addEventListener('ended', function(){
-	//   onEnded(this);
-	// });
 
 	var leftVid = document.getElementById("video-left");
-	// leftVid.addEventListener('ended', function(){
-	//   onEnded(this);
-	// });
 
 	var left = new Tone.Panner(-1).toMaster();
 	var right = new Tone.Panner(1).toMaster();
 
-	var leftPlayer = new Tone.Player("./ring.mp3").connect(left);
+	var leftPlayer = new Tone.Player("./media/ring.mp3").connect(left);
 	leftPlayer.loop = true;
 
 	var rightPlayer = new Tone.Player().connect(right);
@@ -86,6 +72,24 @@
 	});
 
 	var button = document.getElementById("start-button");
+	var answerButton = document.getElementById("phone-icon");
+	var noButton = document.getElementById("no-icon");
+	var infoBox = document.getElementById("info-box");
+
+	var closeButton = document.getElementById("close-icon");
+
+	closeButton.addEventListener("touchend", function(){
+	  infoBox.style.opacity = "0";
+	  infoBox.style.zIndex = "0";
+	});
+
+
+	var canvas = document.getElementById("touch-detector");
+	canvas.width = noButton.width;
+	canvas.height = noButton.height;
+
+	var ctx = canvas.getContext("2d");
+	ctx.drawImage(answerButton, 0, 0, canvas.width, canvas.height);
 
 	button.addEventListener("click", function(){
 	  leftPlayer.start();
@@ -94,7 +98,65 @@
 	  leftVid.play();
 	  leftVid.className = "";
 	  button.style.display = "none";
-	})
+	  
+	  noButton.style.opacity = "1";
+	  canvas.style.opacity = "1";
+	  answerButton.style.zIndex = "100"
+	});
+
+	var isTouched;
+
+	var xOffset = 0;
+	var xInit = 0;
+	var dist = 0;
+
+	answerButton.addEventListener("touchstart", function(e) {
+	  var x = e.touches[0].clientX;
+	  var y = e.touches[0].clientY;
+
+	  if(ctx.getImageData(x, y, 1, 1).data[0] > 0){
+	    isTouched = true;
+	    xInit = x;
+	    xOffset = x;
+	  }
+
+	});
+
+	answerButton.addEventListener("touchmove", function(e){
+	  if(isTouched){
+	    xOffset = e.touches[0].clientX;
+	    dist = xOffset - xInit;
+	  }
+	});
+
+	answerButton.addEventListener("touchend", function(e){
+	  isTouched = false;
+	  // xOffset = 0;
+	  // xInit = 0;
+	});
+
+	function draw(){
+	  if(dist > 10 && !isTouched){
+	    dist -= 10;
+	  } else if(dist <= 10 && !isTouched){
+	    xOffset = 0;
+	    xInit = 0;
+	    dist = 0;
+	  }
+	  ctx.clearRect(0, 0, canvas.width, canvas.height);
+	  ctx.drawImage(answerButton, dist, 0, canvas.width, canvas.height);
+	  if(dist/canvas.width > 0.512){
+	    // window.location = "http://www.sethkranzler.com";
+	    dist = 0.512 * canvas.width;
+	    infoBox.style.opacity = "1";
+	    closeButton.style.zIndex = "101";
+	  }
+	}
+
+	setInterval(function() {
+	  draw();
+	}, 1000/60);
+
 
 /***/ },
 /* 1 */
--- js/main.js
+++ js/main.js
@@ -1,28 +1,14 @@
 var Tone = require("tone");
 
-// function onEnded(that){
-//   that.play();
-//   that.className = "shake shake-constant";
-//   setTimeout(function(){
-//     that.className = "";
-//   }.bind(that), 300);
-// }
-
 var rightVid = document.getElementById("video-right");
 rightVid.playbackRate = 0.99;
-// rightVid.addEventListener('ended', function(){
-//   onEnded(this);
-// });
 
 var leftVid = document.getElementById("video-left");
-// leftVid.addEventListener('ended', function(){
-//   onEnded(this);
-// });
 
 var left = new Tone.Panner(-1).toMaster();
 var right = new Tone.Panner(1).toMaster();
 
-var leftPlayer = new Tone.Player("./ring.mp3").connect(left);
+var leftPlayer = new Tone.Player("./media/ring.mp3").connect(left);
 leftPlayer.loop = true;
 
 var rightPlayer = new Tone.Player().connect(right);
@@ -40,6 +26,24 @@ Tone.Buffer.on("load", function(){
 });
 
 var button = document.getElementById("start-button");
+var answerButton = document.getElementById("phone-icon");
+var noButton = document.getElementById("no-icon");
+var infoBox = document.getElementById("info-box");
+
+var closeButton = document.getElementById("close-icon");
+
+closeButton.addEventListener("touchend", function(){
+  infoBox.style.opacity = "0";
+  infoBox.style.zIndex = "0";
+});
+
+
+var canvas = document.getElementById("touch-detector");
+canvas.width = noButton.width;
+canvas.height = noButton.height;
+
+var ctx = canvas.getContext("2d");
+ctx.drawImage(answerButton, 0, 0, canvas.width, canvas.height);
 
 button.addEventListener("click", function(){
   leftPlayer.start();
@@ -48,4 +52,61 @@ button.addEventListener("click", function(){
   leftVid.play();
   leftVid.className = "";
   button.style.display = "none";
-})
+  
+  noButton.style.opacity = "1";
+  canvas.style.opacity = "1";
+  answerButton.style.zIndex = "100"
+});
+
+var isTouched;
+
+var xOffset = 0;
+var xInit = 0;
+var dist = 0;
+
+answerButton.addEventListener("touchstart", function(e) {
+  var x = e.touches[0].clientX;
+  var y = e.touches[0].clientY;
+
+  if(ctx.getImageData(x, y, 1, 1).data[0] > 0){
+    isTouched = true;
+    xInit = x;
+    xOffset = x;
+  }
+
+});
+
+answerButton.addEventListener("touchmove", function(e){
+  if(isTouched){
+    xOffset = e.touches[0].clientX;
+    dist = xOffset - xInit;
+  }
+});
+
+answerButton.addEventListener("touchend", function(e){
+  isTouched = false;
+  // xOffset = 0;
+  // xInit = 0;
+});
+
+function draw(){
+  if(dist > 10 && !isTouched){
+    dist -= 10;
+  } else if(dist <= 10 && !isTouched){
+    xOffset = 0;
+    xInit = 0;
+    dist = 0;
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(answerButton, dist, 0, canvas.width, canvas.height);
+  if(dist/canvas.width > 0.512){
+    // window.location = "http://www.sethkranzler.com";
+    dist = 0.512 * canvas.width;
+    infoBox.style.opacity = "1";
+    closeButton.style.zIndex = "101";
+  }
+}
+
+setInterval(function() {
+  draw();
+}, 1000/60);
